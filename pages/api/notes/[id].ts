63220faa67c098292fb22b78697a983f4620eb92
Jan Bauer <bauer.jan123@gmail.com>
2021-08-02 09:38:43 +0200

    Added search route to API

--- pages/api/notes/[id].ts
+++ pages/api/notes/[id].ts
@@ -1,7 +1,7 @@
 import {Note} from "@prisma/client";
 import {NextApiRequest, NextApiResponse} from "next";
 import {getSession} from "next-auth/client";
-import {deleteNote} from "../../../repositories/NoteRepository";
+import {deleteNote, searchNotes} from "../../../repositories/NoteRepository";
 import {cRestMethods} from "../../../utils/restAPI";
 
 type Data = {
@@ -17,12 +17,16 @@ export default async function handler(
 
   if (session) {
     const {
-      query: { id, name },
+      query: { id, query, tagId },
       method,
       body,
     } = req;
 
     switch (method) {
+      case cRestMethods.GET:
+        const notes: Note[] = await searchNotes(query as string, session, tagId ? tagId as string : undefined);
+        res.status(200).json(notes);
+        break;
       case cRestMethods.DELETE:
         await deleteNote(id as string);
         res.status(200).json({ message: "Note deleted." });
