379c42bedb97cf9c874a933ffc05e3792fc5d986
Jan Bauer <bauer.jan123@gmail.com>
2021-08-02 18:04:41 +0200

    Setup credentials auth

--- pages/auth/signin/index.tsx
+++ pages/auth/signin/index.tsx
@@ -1,90 +1,74 @@
-import {
-  ClientSafeProvider,
-  getProviders,
-  getSession,
-  signIn,
-} from "next-auth/client";
+import {ClientSafeProvider, getProviders, getSession, signIn,} from "next-auth/client";
 import KeepLogo from "../../../resources/assets/keep.svg";
-import { Button, IconButton } from "@material-ui/core";
+import {Button, Divider, InputAdornment, TextField} from "@material-ui/core";
 import Head from "next/head";
-import React from "react";
-import {
-  SignInPageContainer,
-  SignInPageForm,
-  SignInPageHeadline,
-  SignInPageLogo,
-} from "./sigin.styles";
+import React, {useState} from "react";
+import {SignInPageContainer, SignInPageForm, SignInPageHeadline, SignInPageLogo,} from "./sigin.styles";
 import GitHubIcon from "@material-ui/icons/GitHub";
-import {
-  GetServerSideProps,
-  GetServerSidePropsContext,
-  GetServerSidePropsResult,
-} from "next";
+import {GetServerSidePropsContext, GetServerSidePropsResult} from "next";
+import PersonOutlineOutlinedIcon from "@material-ui/icons/PersonOutlineOutlined";
 
 export interface SignInProps {
   providers: Record<string, ClientSafeProvider> | null;
 }
 
 export default function SignInPage({ providers }: SignInProps) {
+  const [userName, setUserName] = useState("");
+
   const renderForm = () => {
     return (
-      <SignInPageForm
-      // onKeyDown={event => event.keyCode === 13 && handleAuthenticate()}
-      >
+      <SignInPageForm>
         <SignInPageLogo>
           <KeepLogo />
         </SignInPageLogo>
         <SignInPageHeadline>Notes</SignInPageHeadline>
-        {/*<TextField*/}
-        {/*    margin={"normal"}*/}
-        {/*    label={"Username"}*/}
-        {/*    placeholder={"Insert username"}*/}
-        {/*    variant={"standard"}*/}
-        {/*    fullWidth={true}*/}
-        {/*    size={"small"}*/}
-        {/*    InputProps={{*/}
-        {/*      startAdornment: (*/}
-        {/*          <InputAdornment position={"start"}>*/}
-        {/*            <PersonOutlineOutlinedIcon fontSize={"small"} />*/}
-        {/*          </InputAdornment>*/}
-        {/*      ),*/}
-        {/*    }}*/}
-        {/*    onChange={event => dispatch(SignInAPI.handleChangeUserName(event.target.value))}*/}
-        {/*    value={usernameInput}*/}
-        {/*/>*/}
-
-        {/*<TextField*/}
-        {/*    margin={"normal"}*/}
-        {/*    label={"Password"}*/}
-        {/*    title={"Password"}*/}
-        {/*    placeholder="Insert password"*/}
-        {/*    variant={"standard"}*/}
-        {/*    fullWidth={true}*/}
-        {/*    size={"small"}*/}
-        {/*    InputProps={{*/}
-        {/*      startAdornment: (*/}
-        {/*          <InputAdornment position={"start"}>*/}
-        {/*            <LockOutlinedIcon fontSize={"small"} />*/}
-        {/*          </InputAdornment>*/}
-        {/*      ),*/}
-        {/*    }}*/}
-        {/*    onChange={event => dispatch(SignInAPI.handleChangePassword(event.target.value))}*/}
-        {/*    value={passwordInput}*/}
-        {/*/>*/}
+        <TextField
+          margin={"normal"}
+          label={"Username"}
+          placeholder={"Insert username"}
+          variant={"standard"}
+          fullWidth={true}
+          size={"small"}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position={"start"}>
+                <PersonOutlineOutlinedIcon fontSize={"small"} />
+              </InputAdornment>
+            ),
+          }}
+          onChange={(event) => setUserName(event.target.value)}
+          onKeyDown={(event) => event.keyCode === 13 && signIn("credentials", { userName: userName })}
+          value={userName}
+        />
+        <Button
+          size={"small"}
+          className="mt-2"
+          variant={"outlined"}
+          fullWidth={true}
+          onClick={() =>
+            signIn("credentials", { userName: userName })
+          }
+        >
+          Sign in
+        </Button>
+        <Divider className="w-100 mt-3 bg-dark" variant={"fullWidth"}/>
         {providers &&
-          Object.values(providers).map((provider: ClientSafeProvider) => (
-            <Button
-              key={provider.name}
-              size={"small"}
-              className="mt-2"
-              variant={"outlined"}
-              fullWidth={true}
-              endIcon={<GitHubIcon />}
-              onClick={() => signIn(provider.id)}
-            >
-              Sign in with
-            </Button>
-          ))}
+          Object.values(providers).map(
+            (provider: ClientSafeProvider) =>
+              provider.name !== "credentials" && (
+                <Button
+                  key={provider.name}
+                  size={"small"}
+                  className="mt-2"
+                  variant={"outlined"}
+                  fullWidth={true}
+                  endIcon={<GitHubIcon />}
+                  onClick={() => signIn(provider.id)}
+                >
+                  Sign in with
+                </Button>
+              )
+          )}
       </SignInPageForm>
     );
   };
@@ -98,19 +82,6 @@ export default function SignInPage({ providers }: SignInProps) {
       <SignInPageContainer>{renderForm()}</SignInPageContainer>
     </>
   );
-
-  // return (
-  //   <>
-  //
-  //     {Object.values(providers).map((provider: any) => (
-  //       <div key={provider.name}>
-  //         <button onClick={() => signIn(provider.id)}>
-  //           Sign in with {provider.name}
-  //         </button>
-  //       </div>
-  //     ))}
-  //   </>
-  // );
 }
 
 export async function getServerSideProps(
@@ -119,7 +90,7 @@ export async function getServerSideProps(
   const { req, res } = context;
   const session = await getSession({ req });
 
-  if (session && res && session.accessToken) {
+  if (session && res) {
     res.writeHead(302, {
       Location: "/",
     });
